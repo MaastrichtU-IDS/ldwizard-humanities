ed6cd5d9ad83009196b9ab1889ffabb36c025828
Vincent Emonet <vincent.emonet@gmail.com>
2022-04-06 12:57:17 +0200

    add trimming to refineCountryLoc function

--- refineCountryLocation.ts
+++ refineCountryLocation.ts
@@ -6,7 +6,7 @@ const transformation: ColumnRefinement = {
   description:
     "This transformation will generate the given URI only when the value of the row is 1, or true.",
   transformation: async (searchTerm: string, param: any) => {
-    if (searchTerm === '1' || searchTerm.toLowerCase() === 'true') {
+    if (searchTerm.trim() === '1' || searchTerm.trim().toLowerCase() === 'true') {
       return param
     }
     return null
